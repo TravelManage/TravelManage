8f1ba1264c1d54124476bd756572834cde3e65cf
spike.karky <angikar.karky@gmail.com>
2015-08-13 12:07:04 -0400

    Company creation

--- www/script/controllers/LoginController.js
+++ www/script/controllers/LoginController.js
@@ -2,6 +2,10 @@ module.controller('LoginController', function($scope) {
     $scope.login = function(){
         app.mainNav.pushPage("pages/home.html");
     }
+
+    $scope.create = function(){
+            app.mainNav.pushPage("pages/companyEditForm.html");
+        }
 });
 
 /*module.controller('PersonController', function($scope) {
