a662600275efbb861490910f55d8e8f5c6e0fcf7
Kvothe <angikar.karky@gmail.com>
2015-08-19 01:00:16 -0400

    Call

--- www/script/controllers/LoginController.js
+++ www/script/controllers/LoginController.js
@@ -1,14 +1,33 @@
-module.controller('LoginController', function($scope) {
+module.controller('LoginController', function($scope, $http, ServerCalls) {
     $scope.login = function(){
-        app.mainNav.pushPage("pages/home.html");
+
+        var url = appObj.serverLink+ 'loginauth';
+
+        var data = {
+            "username": "hafiz",
+            "password": "gggg"
+        };
+
+        $http({
+            'method': 'POST',
+            'url': url,
+            'data': data,
+            'dataType': 'json'
+        }).success(function(data, status, headers, config) {
+                console.log(data);
+
+            }).error(function(data, status, headers, config) {
+            });
+        //app.mainNav.pushPage("pages/home.html");
+
     }
 
     $scope.create = function(){
-            app.mainNav.pushPage("pages/companyEditForm.html");
-        }
+        app.mainNav.pushPage("pages/companyEditForm.html");
+    }
 });
 
 /*module.controller('PersonController', function($scope) {
-    $scope.personListPage ="pages/personList.html";
-});*/
+ $scope.personListPage ="pages/personList.html";
+ });*/
 
